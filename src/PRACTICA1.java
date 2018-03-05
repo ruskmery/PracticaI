be6d189e9b72905d853067b01ce2ecc6b034fb14
Ruskmery <ruskme.g7@hotmail.com>
2018-03-05 16:05:57 +0000

    mi primer comit

---
+++ src/PRACTICA1.java
@@ -0,0 +1,12 @@
+
+public class PRACTICA1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenido al sistema");
+		System.out.println("Hoy es lunes");
+		System.out.println("Son las 08:00 ");
+		System.out.println("Tiene 8 correos sin leer");
+	}
+
+}
